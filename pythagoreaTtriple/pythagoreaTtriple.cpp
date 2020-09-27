e1e5fd6a7d905ea8c7ad80b6bb4b9a869fde8b47
kaleriya <kaleriya@github.com>
2020-09-27 17:34:47 +0300

    Добавьте файлы проекта.

---
+++ pythagoreaTtriple/pythagoreaTtriple.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int  x, y, z, counter = 0; //элементы последовательности
+
+	std::cout << "Вводите элементы последовательности для нахождения Пифагоровых троек: " << std::endl;
+	std::cout << "Ноль - конец окончания ввода последовательности." << std::endl;
+
+	std::cin >> x;
+	if (std::cin.good() && x != 0 && x != '\n')
+	{
+		std::cin >> y;
+		if (std::cin.good() && y != 0 && y != '\n')
+		{
+			std::cin >> z;
+			if (std::cin.good() && z != 0 && z != '\n')
+			{
+			}
+			else
+			{
+				std::cerr << "Некорректный ввод!" << std::endl;
+				return 1;
+			}
+		}
+		else
+		{
+			std::cerr << "Некорректный ввод!" << std::endl;
+			return 1;
+		}
+	}
+	else
+	{
+		std::cerr << "Некорректный ввод!" << std::endl;
+		return 1;
+	}
+
+	if (x * x + y * y == z * z)
+	{
+		counter++;
+	}
+	while (true) {
+		x = y;
+		y = z;
+		std::cin >> z;
+		if (!std::cin.good())
+		{
+			std::cerr << "Некорректный ввод!" << std::endl;
+			return 1;
+		}
+		if (z == 0)
+		{
+			break;
+		}
+		if (x * x + y * y == z * z)
+		{
+			counter++;
+		}
+	}
+	std::cout << std::endl << "Всего пифагоровых троек: " << counter << std::endl;
+
+	return 0;
+}
